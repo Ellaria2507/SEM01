ddf18ea3e06646f8de5b5d13c3b5cded2d3ed6f7
Nelly Cherniaeva <nelly2507ch@gmail.com>
2023-02-28 22:37:40 +0300

    task02 added

---
+++ seminar02/task02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100){
+    Console.WriteLine("нет 3 числа");
+}
+else{
+    while (number > 999){
+        number = number / 10;  
+    }
+    int b = number % 10;
+    Console.WriteLine(b);
+}
