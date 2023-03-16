192435fe1cf283d9a2fddd12a434c60251cedc3f
Nelly Cherniaeva <nelly2507ch@gmail.com>
2023-03-16 16:26:58 +0300

    ex2 done

--- SEM1.0/seminar06/tasknumber1/Program.cs
+++ SEM1.0/seminar06/tasknumber1/Program.cs
@@ -24,13 +24,21 @@
 //     return sum;
 // }
 
-int[] SUM = NewArray();
-int summ = SumNumber(SUM);
-Console.WriteLine(summ);
+// int[] SUM = NewArray();
+// int summ = SumNumber(SUM);
+// Console.WriteLine(summ);
 
 
 
-Напишите программу, которая найдёт точку пересечения двух прямых, 
-заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.Clear();
+Console.WriteLine("Введите координаты точки ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = (b2-b1)/(k1-k2); 
+double y = k1 * x + b1;
+Console.WriteLine($"{x}, {y}");
