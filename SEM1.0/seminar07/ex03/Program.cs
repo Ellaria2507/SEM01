325eb27c10f4d7a1674f0104af5e884edd7c3aea
Nelly Cherniaeva <nelly2507ch@gmail.com>
2023-03-23 16:10:40 +0300

    ex03 added

---
+++ SEM1.0/seminar07/ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. 
+int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+double[] avrg = new double[array.GetLength(1)];
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        sum += array[j, i];
+    }
+    avrg[i] = sum / array.GetLength(0);
+}
+
+Console.WriteLine(String.Join("; ", avrg));
