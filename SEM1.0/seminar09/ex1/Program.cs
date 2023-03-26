16cfe8166570cf290fd2d894a3625c95664adadb
Nelly Cherniaeva <nelly2507ch@gmail.com>
2023-03-26 13:23:40 +0300

    ex1 added

---
+++ SEM1.0/seminar09/ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Enter number ");
+int n = int.Parse(Console.ReadLine());
+GetNumber(n);
+void GetNumber(int n)
+{
+    if(n == 0)
+    {
+        return;
+    }
+    Console.Write(n + " ");
+    GetNumber(n - 1);
+}
+
